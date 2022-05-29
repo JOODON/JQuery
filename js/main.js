eedf4dca101fea38e8a7d5d7c76b728c8558b754
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-29 23:04:22 +0900

    05.29 Jqury

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-$("h1").css({"color":"red"});
-$("#typo .inner").css({
-  "font-size":"300px",
-  "background":"darkslateblue"
-});
+  //기본적인 선택자 선택 및 css선택
+  $("h1").css({"color":"red"});
+  $("#typo .inner").css({
+  "font-size":"200px",
+  "background":"#ada4e3",
+    "color":"#3f585d"
+  });
+  //이벤트 속성 마우스를 올리거나 땔 경우
   $("#typo .inner").on('mouseover',function (){
-    $(this).css({"background":"red"});
+    $(this).css({"background":"#ec7897"});
     });
-    $("#typo .inner").on('mouseout',function (){
-      $(this).css({"background":"green"});
+
+    $("#typo .inner").stop(true).on('mouseout',function (){
+      $(this).css({"background":"#b1ee9d"});
+    });
+    //클릭 할경우 애니메이션 실행
+    $("#typo .inner").click(function (){
+    $(this).stop(1).animate({opacity: 0,fontSize:'0px'},1500,"swing",
+      function (){
+        (this).animate({opacity: 1,fontSize:'200px'},500)
+      });
     });
 
 
